33e646403b4b375d810679870f09f2f01c1837ca
Юрий Макаров <winnerer@ya.ru>
2015-01-15 21:38:23 +0500

    New version, fixed indent

--- setup.py
+++ setup.py
@@ -2,24 +2,25 @@
 """ Setup file for PyNginxConfig package """
  
 from distutils.core import setup
-setup(name='pynginxconfig',
-      version='0.3.2',
-      description=' PyNginxConfig - NginX config parser and generator',
-      long_description = "PyNginxConfig is a python module for parsing and generating NginX configuration files. Module can parse blocks and single values of unlimited nesting.",
-      author='Makarov Yurii',
-      author_email='winnerer@yandex.com',
-      url='http://code.google.com/p/pynginxconfig/',
-      #packages=[ 'pynginxconfig', ],
-      py_modules=[ 'pynginxconfig', ],
- 
-      classifiers=(
-          'Development Status :: 4 - Beta',
-          'Environment :: Console',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: POSIX',
-          'Operating System :: Unix',
-          'Programming Language :: Python',
-        ),
-      license="MIT"
-     )
+setup(
+    name='pynginxconfig',
+    version='0.3.4',
+    description=' PyNginxConfig - NginX config parser and generator',
+    long_description = "PyNginxConfig is a python module for parsing and generating NginX configuration files. Module can parse blocks and single values of unlimited nesting.",
+    author='Makarov Yurii',
+    author_email='winnerer@yandex.com',
+    url='http://code.google.com/p/pynginxconfig/',
+    #packages=[ 'pynginxconfig', ],
+    py_modules=[ 'pynginxconfig', ],
+
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+    ),
+    license="MIT",
+)
